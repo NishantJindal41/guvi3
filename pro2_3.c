0ed1b1c2787e6743bfab3e2ce920993b1274cc55
NishantJindal41 <44042222+NishantJindal41@users.noreply.github.com>
2019-04-07 13:12:00 +0530

    Create pro2_3.c

---
+++ pro2_3.c
@@ -0,0 +1,30 @@
+    
+#include <stdio.h>
+int main()
+{
+	int n,q,u[10],v[10],a[10],i,j,k,m=0;
+	scanf("%d %d",&n,&q);
+	for(i=1;i<=n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(k=1;k<=q;k++)
+	{
+	    scanf("%d %d",&u[k],&v[k]);
+	}
+		for(k=1;k<=q;k++)
+	{
+	     m=a[0];
+	    for(i=u[k];i<=v[k];i++)
+	    {
+	       if(a[i]<m)
+	       {
+	       m=a[i];
+	       }
+	      
+	    }
+	   printf("%d\n",m);
+	}
+
+	    return 0;
+}
