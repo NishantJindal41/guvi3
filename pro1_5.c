04bca9539f0e002d7ac23ae9df35dbeb3fb43434
NishantJindal41 <44042222+NishantJindal41@users.noreply.github.com>
2019-04-07 12:17:48 +0530

    Create pro1_5.c

---
+++ pro1_5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    unsigned long long n,aa,b;
+    scanf("%d %d %d",&n,&aa,&b);
+    if(n%2==0)
+    {
+        if(a==b)
+        {
+            if((n/(aa+b))%2!=0)
+                printf("YES");
+            else
+                printf("NO");
+        }
+        else{
+            if((n/(aa+b))%2==0)
+                printf("YES");
+            else
+                printf("NO");
+        }
+    }
+    else
+        printf("NO");
+}
