c0890cf0bc6585df872d962ab8535e08bacf70af
NishantJindal41 <44042222+NishantJindal41@users.noreply.github.com>
2019-04-07 16:53:34 +0530

    Create pro4_9.py

---
+++ pro4_9.py
@@ -0,0 +1,6 @@
+s=input("Enter first string:")
+s=input("Enter second string:")
+if(sorted(s)==sorted(s)):
+      print("The strings are anagrams.")
+else:
+      print("The strings aren't anagrams.")
