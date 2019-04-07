5ea85d27f657c31b5b0f054ad893ee1491bfe6e8
NishantJindal41 <44042222+NishantJindal41@users.noreply.github.com>
2019-04-07 12:19:22 +0530

    Create pro1_6.c

---
+++ pro1_6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int K,a[50],s,no;
+scanf("%d",&no);
+for(K=0;K<no;k++);
+{
+scanf("%d",&a[K]);
+}
+for(K=0;k<no;k++)
+{
+for(s=k+1;s<no;s++)
+{
+if(a[k]==a[s])
+{
+printf("%d\n",a[k]);
+}
+}
+}
+getch();
+}
